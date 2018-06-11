f09eca7655c3fc1e6eaf1f36a1ca68acb94d5674
root <root@localhost.localdomain>
2018-06-11 23:17:06 +0300

    Fix heroku issues

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ import sys
 
 if __name__ == "__main__":
     #Load the Heroku environment
-    from herokuapp.env import load_env
-    load_env(__file__,"onlineshop")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onlineshop.settings")
     try:
         from django.core.management import execute_from_command_line
